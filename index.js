51dbf6283bd31df5a402224fcaa05e7a2c16e9a2
SamTanFL <samsite_tan@hotmail.com>
2020-01-09 17:49:23 +0800

    added a little bit of the colors

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const rgbToHSL = require('./rgbToHSL');
 var hex;
 var rgb;
 var hsl;
-
+var r;
+var b;
+var g;
 
 
 switch (process.argv[2]) {
@@ -14,9 +16,18 @@ switch (process.argv[2]) {
         hsl = rgbToHSL(rgb.r, rgb.g, rgb.b);
         console.log(rgb);
         console.log(hsl);
+        r = rgb.r;
+        g = rgb.g;
+        b = rgb.b;
+        console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, rgb.r+rgb.b+rgb.b);
+
     break;
     case "rgb":
         rgb = [parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5])];
+        r = rgb.r;
+        g = rgb.g;
+        b = rgb.b;
+        console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, rgb.r+rgb.b+rgb.b);
         hex = rgbToHex(parseInt(process.argv[3]), parseInt(process.argv[4]), parseInt(process.argv[5]));
         hsl = rgbToHSL(rgb[0], rgb[1], rgb[2]);
         console.log(hex);
@@ -25,3 +36,9 @@ switch (process.argv[2]) {
     default:
         console.log("Input not recognized");
 }
+
+// set as red
+
+
+// this special console will create colored output
+/*console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, myOutput);*/
